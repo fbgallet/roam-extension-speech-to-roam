b3e681b8c3fbc307b4f27d980ca91d4eb4aa2ea7
Fabrice Gallet <fbgallet@gmail.com>
2023-11-30 13:14:55 +0100

    CSS fixes & jsx refactoring

--- src/components/voiceRecorder.js
+++ src/components/voiceRecorder.js
@@ -258,12 +258,7 @@ function VoiceRecorder(props) {
             style={{ color: "#e00000" }}
           />
         ) : (
-          <FontAwesomeIcon
-            icon={faMicrophone}
-            style={{
-              color: "#5c7080",
-            }}
-          />
+          <FontAwesomeIcon icon={faMicrophone} />
         )}
       </>
     );
@@ -297,11 +292,11 @@ function VoiceRecorder(props) {
         style={{ marginRight: isListening ? "0" : "inherit" }}
       >
         <span
-          // onKeyDown={handleKeys}
           class="bp3-icon bp3-icon-shop icon bp3-icon-small"
           style={{
             marginLeft: "2px",
             minWidth: "24px",
+            padding: "0",
           }}
           {...props}
         >
@@ -324,14 +319,14 @@ function VoiceRecorder(props) {
   const timerProps = {
     onClick: handleBackward,
     tabindex: "0",
-    style: { minWidth: "65px" },
+    style: { minWidth: "55px" },
     title: "Rewind and delete the current recording.",
   };
 
   const timerContent = () => {
     return (
       <>
-        <FontAwesomeIcon icon={faBackwardStep} style={{ color: "#5c7080" }} />
+        <FontAwesomeIcon icon={faBackwardStep} />
         <Timer time={time} />
       </>
     );
@@ -363,6 +358,26 @@ function VoiceRecorder(props) {
     );
   };
 
+  const jsxCommandIcon = (props, command, insertIconCallback) => {
+    return (
+      <span class="bp3-popover-wrapper">
+        <span aria-haspopup="true" class="bp3-popover-target">
+          {(isListening || recording !== null) && (
+            <span
+              onClick={command}
+              disabled={!recording}
+              class="bp3-button bp3-minimal bp3-small"
+              tabindex="0"
+              {...props}
+            >
+              {insertIconCallback()}
+            </span>
+          )}
+        </span>
+      </span>
+    );
+  };
+
   return (
     <>
       {position === "left"
@@ -372,71 +387,45 @@ function VoiceRecorder(props) {
         (position === "left"
           ? jsxLogTimerWrapper(timerProps)
           : jsxBp3TimerWrapper(timerProps))}
-      {isToDisplay.transcribeIcon && (
-        <span class="bp3-popover-wrapper">
-          <span aria-haspopup="true" class="bp3-popover-target">
-            {(isListening || recording !== null) && (
-              <span
-                onClick={handleTranscribe}
-                disabled={!recording}
-                class="bp3-button bp3-minimal bp3-small"
-                tabindex="0"
-                title="Transcribe Voice to Text"
+      {isToDisplay.transcribeIcon &&
+        jsxCommandIcon(
+          { title: "Transcribe Voice to Text" },
+          handleTranscribe,
+          () => (
+            <>
+              <FontAwesomeIcon icon={faWandMagicSparkles} />
+            </>
+          )
+        )}{" "}
+      {isToDisplay.translateIcon &&
+        jsxCommandIcon(
+          { title: "Translate Voice to English text" },
+          handleTranslate,
+          () => (
+            <>
+              <FontAwesomeIcon icon={faLanguage} flip="horizontal" />
+            </>
+          )
+        )}
+      {isToDisplay.completionIcon &&
+        jsxCommandIcon(
+          {
+            title: "Transcribe and use as prompt for a ChatGPT",
+            style: { minWidth: "30px" },
+          },
+          handleCompletion,
+          () => (
+            <>
+              <svg
+                role="img"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <FontAwesomeIcon
-                  icon={faWandMagicSparkles}
-                  style={{ color: "#5c7080" }}
-                />
-              </span>
-            )}
-          </span>
-        </span>
-      )}{" "}
-      {isToDisplay.translateIcon && (
-        <span class="bp3-popover-wrapper">
-          <span aria-haspopup="true" class="bp3-popover-target">
-            {(isListening || recording !== null) && (
-              <span
-                onClick={handleTranslate}
-                disabled={!recording}
-                class="bp3-button bp3-minimal bp3-small"
-                tabindex="0"
-                title="Translate Voice to English text"
-              >
-                <FontAwesomeIcon
-                  icon={faLanguage}
-                  flip="horizontal"
-                  style={{ color: "#5c7080" }}
-                />
-              </span>
-            )}
-          </span>
-        </span>
-      )}
-      {isToDisplay.completionIcon && (
-        <span class="bp3-popover-wrapper">
-          <span aria-haspopup="true" class="bp3-popover-target">
-            {(isListening || recording !== null) && (
-              <span
-                onClick={handleCompletion}
-                disabled={!recording}
-                class="bp3-button bp3-minimal bp3-small"
-                tabindex="0"
-                title="Transcribe and use as prompt for a GPT model."
-                style={{ minWidth: "30px" }}
-              >
-                <svg
-                  role="img"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M22.282 9.821a5.985 5.985 0 0 0-.516-4.91 6.046 6.046 0 0 0-6.51-2.9A6.065 6.065 0 0 0 4.981 4.18a5.985 5.985 0 0 0-3.998 2.9 6.046 6.046 0 0 0 .743 7.097 5.98 5.98 0 0 0 .51 4.911 6.051 6.051 0 0 0 6.515 2.9A5.985 5.985 0 0 0 13.26 24a6.056 6.056 0 0 0 5.772-4.206 5.99 5.99 0 0 0 3.997-2.9 6.056 6.056 0 0 0-.747-7.073zM13.26 22.43a4.476 4.476 0 0 1-2.876-1.04l.141-.081 4.779-2.758a.795.795 0 0 0 .392-.681v-6.737l2.02 1.168a.071.071 0 0 1 .038.052v5.583a4.504 4.504 0 0 1-4.494 4.494zM3.6 18.304a4.47 4.47 0 0 1-.535-3.014l.142.085 4.783 2.759a.771.771 0 0 0 .78 0l5.843-3.369v2.332a.08.08 0 0 1-.033.062L9.74 19.95a4.5 4.5 0 0 1-6.14-1.646zM2.34 7.896a4.485 4.485 0 0 1 2.366-1.973V11.6a.766.766 0 0 0 .388.676l5.815 3.355-2.02 1.168a.076.076 0 0 1-.071 0l-4.83-2.786A4.504 4.504 0 0 1 2.34 7.872zm16.597 3.855l-5.833-3.387L15.119 7.2a.076.076 0 0 1 .071 0l4.83 2.791a4.494 4.494 0 0 1-.676 8.105v-5.678a.79.79 0 0 0-.407-.667zm2.01-3.023l-.141-.085-4.774-2.782a.776.776 0 0 0-.785 0L9.409 9.23V6.897a.066.066 0 0 1 .028-.061l4.83-2.787a4.5 4.5 0 0 1 6.68 4.66zm-12.64 4.135l-2.02-1.164a.08.08 0 0 1-.038-.057V6.075a4.5 4.5 0 0 1 7.375-3.453l-.142.08L8.704 5.46a.795.795 0 0 0-.393.681zm1.097-2.365l2.602-1.5 2.607 1.5v2.999l-2.597 1.5-2.607-1.5z" />
-                </svg>
-              </span>
-            )}
-          </span>
-        </span>
-      )}
+                <path d="M22.282 9.821a5.985 5.985 0 0 0-.516-4.91 6.046 6.046 0 0 0-6.51-2.9A6.065 6.065 0 0 0 4.981 4.18a5.985 5.985 0 0 0-3.998 2.9 6.046 6.046 0 0 0 .743 7.097 5.98 5.98 0 0 0 .51 4.911 6.051 6.051 0 0 0 6.515 2.9A5.985 5.985 0 0 0 13.26 24a6.056 6.056 0 0 0 5.772-4.206 5.99 5.99 0 0 0 3.997-2.9 6.056 6.056 0 0 0-.747-7.073zM13.26 22.43a4.476 4.476 0 0 1-2.876-1.04l.141-.081 4.779-2.758a.795.795 0 0 0 .392-.681v-6.737l2.02 1.168a.071.071 0 0 1 .038.052v5.583a4.504 4.504 0 0 1-4.494 4.494zM3.6 18.304a4.47 4.47 0 0 1-.535-3.014l.142.085 4.783 2.759a.771.771 0 0 0 .78 0l5.843-3.369v2.332a.08.08 0 0 1-.033.062L9.74 19.95a4.5 4.5 0 0 1-6.14-1.646zM2.34 7.896a4.485 4.485 0 0 1 2.366-1.973V11.6a.766.766 0 0 0 .388.676l5.815 3.355-2.02 1.168a.076.076 0 0 1-.071 0l-4.83-2.786A4.504 4.504 0 0 1 2.34 7.872zm16.597 3.855l-5.833-3.387L15.119 7.2a.076.076 0 0 1 .071 0l4.83 2.791a4.494 4.494 0 0 1-.676 8.105v-5.678a.79.79 0 0 0-.407-.667zm2.01-3.023l-.141-.085-4.774-2.782a.776.776 0 0 0-.785 0L9.409 9.23V6.897a.066.066 0 0 1 .028-.061l4.83-2.787a4.5 4.5 0 0 1 6.68 4.66zm-12.64 4.135l-2.02-1.164a.08.08 0 0 1-.038-.057V6.075a4.5 4.5 0 0 1 7.375-3.453l-.142.08L8.704 5.46a.795.795 0 0 0-.393.681zm1.097-2.365l2.602-1.5 2.607 1.5v2.999l-2.597 1.5-2.607-1.5z" />
+              </svg>
+            </>
+          )
+        )}
     </>
   );
 }
